784444dc75cec91a9161c50266e5479e6c21ded9
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-06-18 15:38:38 +0530

    Added validations to Location model

--- backpiperz/app/models/location.rb
+++ backpiperz/app/models/location.rb
@@ -1,2 +1,13 @@
 class Location < ActiveRecord::Base
+  has_many :nearby_locations, class_name: 'Location', foreign_key: 'parent_location_id'
+
+  belongs_to :landmark, class_name: 'Location'
+
+  validates :name, presence: true
+  validates :location_type, presence: true
+  private
+
+  def self.table
+    arel_table
+  end
 end
