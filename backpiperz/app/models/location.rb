8f13fc06e434df9279c0ab0c1571eb1ca7747bb3
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-06-19 10:19:42 +0530

    Joins - Work in progress

--- backpiperz/app/models/location.rb
+++ backpiperz/app/models/location.rb
@@ -6,6 +6,14 @@ class Location < ActiveRecord::Base
 
   validates :name, presence: true
   validates :location_type, presence: true
+
+  def self.with_reviews_if_available
+    # Left outer join
+    joins(table.join(Review.table, Arel::OuterJoin).on(table[:id].eq(Review.table[:location_id])).join_sources)
+    # Innter join with extra join conditions
+    # joins(table.join(Review.table).on(table[:id].eq(Review.table[:location_id].and(Review.rating_more_than_3)).join_sources))
+  end
+
   private
 
   def self.table
--- backpiperz/app/models/review.rb
+++ backpiperz/app/models/review.rb
@@ -4,4 +4,14 @@ class Review < ActiveRecord::Base
 
   belongs_to :traveler
   belongs_to :location
+
+  def self.rating_more_than_3
+    table[:rating].gt(3)
+  end
+
+  private
+
+  def self.table
+    arel_table
+  end
 end
