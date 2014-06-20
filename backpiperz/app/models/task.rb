5c5e018cfc8a4ea9c8a49e068763debe292092a7
Vipul A M <vipulnsward@gmail.com>
2014-06-20 12:09:44 +0530

    Rename App

--- backpiperz/app/models/task.rb
+++
@@ -1,5 +0,0 @@
-class Task < ActiveRecord::Base
-  belongs_to :location
-
-  validates :name, presence: true
-end
--- backpiperz/app/models/traveler.rb
+++
@@ -1,5 +0,0 @@
-class Traveler < ActiveRecord::Base
-  has_many :reviews
-
-  validates :name, presence: true
-end
