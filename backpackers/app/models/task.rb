605afe01ad72155b9a7ae9dc967aed074b97b402
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-06-20 21:08:25 +0530

    Added named function for ordering tasks based on completed_at or created_at

--- backpackers/app/models/task.rb
+++ backpackers/app/models/task.rb
@@ -2,4 +2,14 @@ class Task < ActiveRecord::Base
   belongs_to :location
 
   validates :name, presence: true
+
+  def self.order_criteria
+    Arel::Nodes::NamedFunction.new('coalesce', ['completed_at', 'created_at'])
+  end
+
+  private
+
+  def self.table
+    arel_table
+  end
 end
