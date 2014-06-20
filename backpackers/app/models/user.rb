687b0dba5143fed0ab73e8e6015a6cce42556e63
Vipul A M <vipulnsward@gmail.com>
2014-06-20 23:51:12 +0530

    Add encrypt function example

---
+++ backpackers/app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ActiveRecord::Base
+
+  # RAW
+  def set_sekret(value = 'lol', key = 'sekret')
+    escape_and_execute(["SELECT pgp_sym_encrypt(?, ?)", value.to_s, key])['pgp_sym_encrypt']
+  end
+
+  def get_sekret(value = 'lol', key = '\xc30d04070302ead63c9e04cc6e3e7ed23401ba88bf7a71d971f1a58dcd4389ecfc0d17fb7b7d72d924a953efb432a44fdeca57a2046ea3276b7d7e77543a335c5d126913bb')
+    escape_and_execute(["SELECT pgp_sym_decrypt(?, ?)", value, key])['pgp_sym_decrypt']
+  rescue
+    raise 'lol; get it right bro'
+  end
+
+  def escape_and_execute(query)
+    query = ::ActiveRecord::Base.send :sanitize_sql_array, query
+    ::ActiveRecord::Base.connection.execute(query).first
+  end
+
+  #Arel
+  def arel_sekret(value = 'lol', key = 'sekret')
+    select_from_table pgp_sym_encrypt(value, key)
+  end
+
+  def pgp_sym_encrypt(value = 'lol', key = 'sekret')
+    Arel::Nodes::NamedFunction.new('pgp_sym_encrypt', [value, key])
+  end
+
+  def select_from_table arel_nodes
+    User.arel_table.project arel_nodes
+  end
+end
--- backpackers/db/schema.rb
+++ backpackers/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140620153051) do
+ActiveRecord::Schema.define(version: 20140620173158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 20140620153051) do
 
   create_table "travelers", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "name"
+    t.string   "sekret"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
