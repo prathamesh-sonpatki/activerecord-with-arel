78626436c3e61c901a99186d9f097e63f8f7a797
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-06-21 09:57:28 +0530

    Added example for self join with extra join condition of distance

--- backpackers/app/models/location.rb
+++ backpackers/app/models/location.rb
@@ -80,6 +80,34 @@ class Location < ActiveRecord::Base
     # SELECT locations.* FROM locations INNER JOIN locations locations_2 ON locations.id = locations_2.parent_location_id
   end
 
+  # All nearby locations within 5 km from parent
+  # RAW
+  def self.all_nearby_locations_within_distance_from_parent_raw
+    joins("INNER JOIN locations nearby_locations
+           ON locations.id = nearby_locations.parent_location_id
+           AND nearby_locations.distance_from_parent <= #{5}"
+    )
+    # SELECT "locations".* FROM "locations" INNER JOIN locations nearby_locations
+    # ON locations.id = nearby_locations.parent_location_id
+    # AND nearby_locations.distance_from_parent <= 5
+  end
+
+  # Arel
+  def self.all_nearby_locations_within_distance_from_parent
+    nearby_locations = table.alias
+    joins(table.join(nearby_locations)
+      .on(table[:id].eq(nearby_locations[:parent_location_id])
+                    .and(nearby_locations[:distance_from_parent].lteq(5)
+                    # .or(nearby_locations[:location_type].eq('hotel'))
+                    # .and(nearby_locations[:location_type].eq('hotel'))
+                    )
+         )
+      .join_sources
+    )
+    # SELECT locations.* FROM locations INNER JOIN locations locations_2
+    # ON locations.id = locations_2.parent_location_id
+    # AND locations_2.distance_from_parent <= 5
+  end
   private
 
   def self.table
