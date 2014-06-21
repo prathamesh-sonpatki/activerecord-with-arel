cc7cce2d5e1c6f16e935a7a6fcd3d8596c7283cd
Vipul A M <vipulnsward@gmail.com>
2014-06-21 12:47:13 +0530

    wip windows

--- backpackers/app/models/review.rb
+++ backpackers/app/models/review.rb
@@ -15,6 +15,21 @@ class Review < ActiveRecord::Base
     table[:rating].gt(rating)
   end
 
+  # DONTREF: Find all reviews, first sorted by rating then by created_at, group on location_id
+  # "id, row_number() OVER (PARTITION BY category_id ORDER BY price ASC, name ASC)"
+  def self.sorted_reviews
+    manager = Arel::SelectManager.new table.engine
+    manager.from table
+    manager.project Arel.star
+    manager.group(table[:location_id])
+
+    # window = Arel::Nodes::Window.new.order(table[:rating].desc, table[:created_at].desc)
+    # table[:rating].count.over(window)
+    manager.window('sorted_reviews').order(table[:rating].desc, table[:created_at].desc)
+    manager
+    #table.over window
+  end
+
   private
 
   def self.table
