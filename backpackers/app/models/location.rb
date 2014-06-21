d04f06e1aa323d2e1740218bd527a45f50841dfd
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-06-21 09:33:38 +0530

    Added raw examples for outer join queries on Location model

--- backpackers/app/models/location.rb
+++ backpackers/app/models/location.rb
@@ -8,29 +8,41 @@ class Location < ActiveRecord::Base
   validates :name, presence: true
   validates :location_type, presence: true
 
+  # Left outer join
+  # RAW
+  def self.with_reviews_if_available_raw
+    joins("LEFT OUTER JOIN reviews ON locations.id = reviews.location_id")
+    # SELECT "locations".* FROM "locations" LEFT OUTER JOIN reviews ON locations.id = reviews.location_id
+  end
+
+  # Arel
   def self.with_reviews_if_available
-    # Left outer join
     joins(
       table.join(Review.table, Arel::OuterJoin)
            .on(table[:id].eq(Review.table[:location_id])
          ).join_sources
          )
+    # SELECT locations.* FROM locations LEFT OUTER JOIN reviews ON locations.id = reviews.location_id
   end
 
+  # Left outer join with extra join condition
+  # RAW
+  def self.with_reviews_having_rating_greater_than_raw(rating = 3)
+    joins("LEFT OUTER JOIN reviews ON locations.id = reviews.location_id AND reviews.rating > #{rating}")
+    # SELECT locations.* FROM locations LEFT OUTER JOIN reviews ON locations.id = reviews.location_id AND reviews.rating > 3
+  end
+
+  # Arel
   def self.with_reviews_having_rating_greater_than(rating = 3)
     joins(
       table.join(Review.table, Arel::OuterJoin).
             on(locations_reviews_join.and(Review.having_rating_more_than(rating)))
             .join_sources
          )
-  end
-
-  def self.locations_reviews_join
-    table[:id].eq(Review.table[:location_id])
+    # SELECT locations.* FROM locations LEFT OUTER JOIN reviews ON locations.id = reviews.location_id AND reviews.rating > 3
   end
 
   # All locations for which there has been booking and review with rating more than 3
-
   # RAW
   def self.all_locations_with_review_and_bookings_raw
     [
@@ -58,5 +70,9 @@ class Location < ActiveRecord::Base
 
   def self.table
     arel_table
+  end
+
+  def self.locations_reviews_join
+    table[:id].eq(Review.table[:location_id])
   end
 end
